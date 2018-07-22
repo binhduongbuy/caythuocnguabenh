58fe6e086f207cc40bc74c5b5c1f604f2a245fbb
Truong Dung <34378419+binhduongbuy@users.noreply.github.com>
2018-07-22 22:06:23 +0700

    1

---
+++ components/SideBar/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Youtube from '../Youtube'
+
+import './style.scss'
+
+class SideBar extends React.Component {
+    render() {
+
+    const AdsenseSponsorAds = buildAdsenseSponsorAds();
+    const AdsenseSponsorAdsEnds = buildAdsenseSponsorAdsEnds();
+
+        return (
+            <div className='sidebar'>
+                <Youtube {...this.props} />
+		<div className='sidebar__sponsor'>Sponsor <br />
+		<script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' />
+		{ process.env.NODE_ENV === 'production' ? AdsenseSponsorAds : null }
+		{ process.env.NODE_ENV === 'production' ? AdsenseSponsorAdsEnds : null }
+		</div>
+                
+            </div>
+        );
+    }
+}
+
+function buildAdsenseSponsorAds() {
+  const js = `
+  <!-- Sponsor Right Sidebar -->
+<ins class='adsbygoogle'
+     style='display:block'
+     data-ad-client='ca-pub-IDHERE'
+     data-ad-slot='8665159676'
+     data-ad-format='auto'></ins>
+  `;
+
+  return <script
+    dangerouslySetInnerHTML={{ __html: js }}
+  />;
+}
+
+function buildAdsenseSponsorAdsEnds() {
+  const js = `
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  `;
+
+  return <script
+    dangerouslySetInnerHTML={{ __html: js }}
+  />;
+}
+
+export default SideBar
