afb6a942ac59cf489f8197c6dcf26a59ce56e448
Truong Dung <34378419+binhduongbuy@users.noreply.github.com>
2018-08-16 01:16:55 +0700

    1

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-5520171158294659',
+    google_ad_client: 'ca-pub-6200342349287235',
     enable_page_level_ads: true
   });
   `;
